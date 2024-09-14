3d1d70e6596d2e5bb7ff232f75ceaa76af9f5d4d
Bot1719744 <frankliu.TJ95@gmail.com>
2024-09-14 15:43:19 -0700

    Update

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,13 +7,23 @@ export default function Labs() {
     return (
         <div>
             <h1>Labs</h1>
-            <TOC />
+
+            <h1>Github Repo</h1>
+            Please
+            <a id="wd-lipsum" href="https://github.com/Bot1719744/kanbas-react-web-app">click here</a>
+            to navigate to my github repo for this project<br/>
+
+            <h1>Dingwen Liu</h1>
+
+            <TOC/>
             <Routes>
-                <Route path="/" element={<Navigate to="Lab1" />} />
-                <Route path="Lab1" element={<Lab1 />} />
-                <Route path="Lab2" element={<Lab2 />} />
-                <Route path="Lab3" element={<Lab3 />} />
+                <Route path="/" element={<Navigate to="Lab1"/>}/>
+                <Route path="Lab1" element={<Lab1/>}/>
+                <Route path="Lab2" element={<Lab2/>}/>
+                <Route path="Lab3" element={<Lab3/>}/>
             </Routes>
+
+
         </div>
     );
 }
