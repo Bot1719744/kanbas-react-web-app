d340e878e37d1f0a42ffc761540c27ff236ec3a5
Bot1719744 <frankliu.TJ95@gmail.com>
2024-10-23 19:33:40 -0700

    Stuck at 3.3

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router";
+import React, { useState } from "react";
 import * as db from "../../Database";
 import { BsGripVertical } from "react-icons/bs";
 import ModulesControls from "./ModulesControls";
@@ -6,15 +7,13 @@ import ModuleControlButtons from "./ModuleControlButtons";
 import LessonControlButtons from "./LessonControlButtons";
 
 export default function Modules() {
-    const { cid } = useParams(); // Get course ID from route params
-    const modules = db.modules; // Fetch modules from the database
+    const { cid } = useParams();
+    const modules = db.modules;
 
     return (
         <div>
-            {/* Modules Controls at the top */}
             <ModulesControls /><br /><br /><br /><br />
 
-            {/* List of Modules */}
             <ul id="wd-modules" className="list-group rounded-0" style={{ marginLeft: "100px", marginRight: "100px" }}>
                 {modules
                     .filter((module) => module.course === cid) // Filter modules by course ID
@@ -22,18 +21,17 @@ export default function Modules() {
                         <li key={module._id} className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
                             <div className="wd-title p-3 ps-2 bg-secondary">
                                 <BsGripVertical className="me-2 fs-3" />
-                                {module.name} {/* Module Name */}
-                                <ModuleControlButtons /> {/* Module Control Buttons */}
+                                {module.name}
+                                <ModuleControlButtons />
                             </div>
 
-                            {/* List of Lessons within each Module */}
                             {module.lessons && (
                                 <ul className="wd-lessons list-group rounded-0">
                                     {module.lessons.map((lesson) => (
                                         <li key={lesson._id} className="wd-lesson list-group-item p-3 ps-1">
                                             <BsGripVertical className="me-2 fs-3" />
-                                            {lesson.name} {/* Lesson Name */}
-                                            <LessonControlButtons /> {/* Lesson Control Buttons */}
+                                            {lesson.name}
+                                            <LessonControlButtons />
                                         </li>
                                     ))}
                                 </ul>
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -8,9 +8,8 @@ import React from "react";
 import { FaAlignJustify } from "react-icons/fa";
 import PeopleTable from "./People/Table";
 import KanbasNavigation from "../Navigation";
-import { courses } from "../Database";
 
-export default function Courses() {
+export default function Courses({ courses }: { courses: any[]; }) {
 
     const { cid } = useParams();
     const course = courses.find((course) => course._id === cid);
